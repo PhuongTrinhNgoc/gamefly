efdf43fa5544cb545f61a1d6f5972ae87e5d3925
PhuongTrinh <phuongxu0398@gmail.com>
2022-11-20 12:28:01 +0700

    fix logo

--- src/component/Header.js
+++ src/component/Header.js
@@ -97,11 +97,25 @@ const Header = () => {
         className={`navbar${sticky.isSticky ? " sticky" : ""} nav`}
       >
         <Logo className="logo">
-          <RotateInDiv style={{ width: "100%", position: "absolute" }}>
+          <RotateInDiv
+            style={{
+              width: "100%",
+              position: "absolute",
+              top: "4px",
+              height: "100%",
+            }}
+          >
             {" "}
             <Img src={src2} />
           </RotateInDiv>
-          <RotateOutDiv style={{ width: "100%", position: "absolute" }}>
+          <RotateOutDiv
+            style={{
+              width: "100%",
+              position: "absolute",
+              top: "4px",
+              height: "100%",
+            }}
+          >
             {" "}
             <Img src={src} />
           </RotateOutDiv>
