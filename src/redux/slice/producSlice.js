763508205d096c4f5c6dffbd6036a8990906ffcc
phuong-trinh <102595122+PhuongTrinhNgoc@users.noreply.github.com>
2024-10-22 08:02:15 +0700

    Update producSlice.js
    fix data

--- src/redux/slice/producSlice.js
+++ src/redux/slice/producSlice.js
@@ -37,7 +37,7 @@ export const getItemsCart = createAsyncThunk(
   async () => {
     try {
       const responsive = await axios.get(
-        "https://firebasestorage.googleapis.com/v0/b/project-game-15c1e.appspot.com/o/my-project-phuong-84bb6-default-rtdb-export.json?alt=media&token=48a91305-2a66-440f-a3c9-bc318ac887a6"
+        "https://project-game-15c1e-default-rtdb.asia-southeast1.firebasedatabase.app/"
       );
 
       return responsive.data;
@@ -51,7 +51,7 @@ export const getitems = createAsyncThunk(
   async (listCateroly) => {
     try {
       let responsive = await axios.get(
-        `https://firebasestorage.googleapis.com/v0/b/project-game-15c1e.appspot.com/o/my-project-phuong-84bb6-default-rtdb-export.json?alt=media&token=48a91305-2a66-440f-a3c9-bc318ac887a6/items.json?orderBy="category"&equalTo="${listCateroly}"`
+        `https://project-game-15c1e-default-rtdb.asia-southeast1.firebasedatabase.app/?orderBy="category"&equalTo="${listCateroly}"`
       );
       const myArr = [];
 
@@ -67,7 +67,7 @@ export const getitems = createAsyncThunk(
 export const getById = createAsyncThunk("products/getById", async (itemId) => {
   try {
     const responsive = await axios.get(
-      `https://firebasestorage.googleapis.com/v0/b/project-game-15c1e.appspot.com/o/my-project-phuong-84bb6-default-rtdb-export.json?alt=media&token=48a91305-2a66-440f-a3c9-bc318ac887a6/items.json?orderBy="_id"&equalTo=${itemId}`
+      `https://project-game-15c1e-default-rtdb.asia-southeast1.firebasedatabase.app/?orderBy="_id"&equalTo=${itemId}`
     );
     const myArr1 = [];
 
@@ -86,7 +86,7 @@ export const getBlogById = createAsyncThunk(
   async (itemBlogId) => {
     try {
       const responsive = await axios.get(
-        `https://firebasestorage.googleapis.com/v0/b/project-game-15c1e.appspot.com/o/my-project-phuong-84bb6-default-rtdb-export.json?alt=media&token=48a91305-2a66-440f-a3c9-bc318ac887a6/blogs.json?orderBy="id"&equalTo=${itemBlogId}`
+        `https://project-game-15c1e-default-rtdb.asia-southeast1.firebasedatabase.app//blogs.json?orderBy="id"&equalTo=${itemBlogId}`
       );
       const myArr2 = [];
 
@@ -104,7 +104,7 @@ export const getPagination = createAsyncThunk(
   async (startEnd) => {
     try {
       const responsive = await axios.get(
-        `https://firebasestorage.googleapis.com/v0/b/project-game-15c1e.appspot.com/o/my-project-phuong-84bb6-default-rtdb-export.json?alt=media&token=48a91305-2a66-440f-a3c9-bc318ac887a6/items.json?orderBy="_id"${startEnd}`
+        `https://project-game-15c1e-default-rtdb.asia-southeast1.firebasedatabase.app/?orderBy="_id"${startEnd}`
       );
 
       const myArr2 = [];
@@ -123,7 +123,7 @@ export const getPaginationGame = createAsyncThunk(
   async (value) => {
     try {
       const responsive = await axios.get(
-        `https://firebasestorage.googleapis.com/v0/b/project-game-15c1e.appspot.com/o/my-project-phuong-84bb6-default-rtdb-export.json?alt=media&token=48a91305-2a66-440f-a3c9-bc318ac887a6/items.json?orderBy="_id"${value}`
+        `https://project-game-15c1e-default-rtdb.asia-southeast1.firebasedatabase.app/?orderBy="_id"${value}`
       );
 
       const myArr2 = [];
